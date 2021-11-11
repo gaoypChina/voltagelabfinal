ee52a7ba8230b030e8a67cf23c83f783283924ea
nazmulislamrony <nazmulisrony@gmail.com>
2021-11-11 23:10:47 +0600

    add helper

---
+++ lib/helper/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Global {
+  static TextStyle titleOfList = GoogleFonts.hindSiliguri(
+      color: Colors.black, fontWeight: FontWeight.w600);
+  static Color defaultColor = Color.fromRGBO(0, 116, 255, 1);
+
+}
